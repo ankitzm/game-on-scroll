8229f666815343460c555adf2a8c9a0d068176c2
ankitzm <ankit.singh.zm@gmail.com>
2022-10-28 14:44:42 +0530

    set up game characters

--- gameContract/contracts/MyGame.sol
+++ gameContract/contracts/MyGame.sol
@@ -5,7 +5,47 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract MyGame {
-    constructor() {
-        console.log("THIS IS MY GAME CONTRACT. NICE.");
+    // character feature struct
+    struct CharacterAttribute {
+        uint characterIndex;
+        string name;
+        string imageURI;
+        uint hp;
+        uint maxHp;
+        uint atttackDamage;
+    }
+
+    // default character list
+    CharacterAttribute[] defaultCharaters;
+
+    // data passed into the contract when it's first created - inititalising
+    constructor(
+        string[] memory charName,
+        string[] memory charImage,
+        uint[] memory charHp,
+        uint[] memory charMaxHp,
+        uint[] memory charAttackDamage
+    ) {
+        // loop through all the characters and save in contract
+        for (uint i = 0; i < charName.length; i += 1) {
+            defaultCharaters.push(
+                CharacterAttribute(
+                    i,
+                    charName[i],
+                    charImage[i],
+                    charHp[i],
+                    charMaxHp[i],
+                    charAttackDamage[i]
+                )
+            );
+
+            CharacterAttribute memory c = defaultCharaters[i];
+            console.log(
+                "Initialised % with %s, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
+        }
     }
 }
--- gameContract/scripts/run.js
+++ gameContract/scripts/run.js
@@ -1,6 +1,16 @@
 const main = async () => {
 	const gameContractFactory = await hre.ethers.getContractFactory("MyGame")
-	const gameContract = await gameContractFactory.deploy()
+	const gameContract = await gameContractFactory.deploy(
+		["Leo", "Aang", "Pikachu"],
+		[
+			"https://i.imgur.com/pKd5Sdk.png",
+			"https://i.imgur.com/xVu4vFL.png",
+			"https://i.imgur.com/WMB6g9u.png",
+		],
+		[100, 200, 300],
+		[500, 500, 500],
+		[100, 50, 25],
+	)
 	await gameContract.deployed()
 
 	console.log("contract deployed at :", gameContract.address)
