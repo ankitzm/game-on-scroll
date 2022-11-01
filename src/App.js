0004a9230e00adb209e622ee0f27dd2b724818a3
ankitzm <ankit.singh.zm@gmail.com>
2022-11-02 01:16:46 +0530

    metamsk login added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import twitterLogo from "./assets/twitter-logo.svg"
 import "./App.css"
 
@@ -7,21 +7,77 @@ const TWITTER_HANDLE = "_buildspace"
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
+	const [curresntAccount, setCurrentAccount] = useState(null)
+
+	const checkWalletConnection = async () => {
+		try {
+			const { ethereum } = window
+
+			if (!ethereum) {
+				console.log("make sure you have Metamsk")
+			} else {
+				console.log("we got the ethereum object !!", ethereum)
+
+				const accounts = await ethereum.request({
+					method: "eth_accounts",
+				})
+
+				if (accounts.length !== 0) {
+					const account = accounts[0]
+					console.log("Found an authorised account - ", account)
+					setCurrentAccount(account)
+				} else {
+					console.log("Can't find authorised account")
+				}
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	// connect wallet
+	const connectWallet = async () => {
+		try {
+			const { ethereum } = window
+
+			if (!ethereum) {
+				console.log("make sure you have Metamsk")
+				return
+			}
+
+			const accounts = await ethereum.request({
+				method: "eth_requestAccounts",
+			})
+
+			console.log("Connected", accounts[0])
+			setCurrentAccount(accounts[0])
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	// runs the function once the page loads
+	useEffect(() => {
+		checkWalletConnection()
+	}, [])
+
 	return (
 		<div className="App">
 			<div className="container">
 				<div className="header-container">
-					<p className="header gradient-text">
-						⚔️ Metaverse Slayer ⚔️
-					</p>
-					<p className="sub-text">
-						Team up to protect the Metaverse!
-					</p>
+					<p className="header gradient-text">⚔️ ENDGAME ⚔️</p>
+					<p className="sub-text">Avengers in the Metaverse</p>
 					<div className="connect-wallet-container">
 						<img
 							src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
 							alt="Monty Python Gif"
 						/>
+						<button
+							className="cta-button connect-wallet-button"
+							onClick={connectWallet}
+						>
+							Connect Wallet To Get Started
+						</button>
 					</div>
 				</div>
 				<div className="footer-container">
