afba58fba779fcb0c5e0aac97cc2bbc425b60c71
ankitzm <ankit.singh.zm@gmail.com>
2022-11-01 23:49:13 +0530

    added deply script

--- gameContract/hardhat.config.js
+++ gameContract/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { ALCHEMY_API_URL, PRIVATE_GOERLI_ACCOUNT_KEY } = process.env
+
 module.exports = {
-  solidity: "0.8.17",
-};
+	solidity: "0.8.17",
+	networks: {
+		goerli: {
+			url: ALCHEMY_API_URL,
+			accounts: [PRIVATE_GOERLI_ACCOUNT_KEY],
+		},
+	},
+}
