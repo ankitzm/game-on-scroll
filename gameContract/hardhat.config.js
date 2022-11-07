9405a9c49fcebc5a5d6c6f11eecf9716b437617a
ankitzm <ankit.singh.zm@gmail.com>
2022-11-07 22:06:10 +0530

    deployed to polygon testnet(mumbai)

--- gameContract/hardhat.config.js
+++ gameContract/hardhat.config.js
@@ -1,14 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 
-const { ALCHEMY_API_URL, PRIVATE_GOERLI_ACCOUNT_KEY } = process.env
+const { ALCHEMY_API_URL, PRIVATE_ACCOUNT_KEY } = process.env
 
 module.exports = {
 	solidity: "0.8.17",
 	networks: {
-		goerli: {
+		mumbai: {
 			url: ALCHEMY_API_URL,
-			accounts: [PRIVATE_GOERLI_ACCOUNT_KEY],
+			accounts: [PRIVATE_ACCOUNT_KEY],
 		},
 	},
 }
