3d1fc005659f7f2d158a9e048ba26e97ae149127
Ankit Singh <ankit.singh.zm@gmail.com>
2023-03-26 19:23:46 +0530

    added scroll network in hardhat

--- gameContract/hardhat.config.js
+++ gameContract/hardhat.config.js
@@ -1,14 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 
-const { ALCHEMY_API_URL, PRIVATE_ACCOUNT_KEY } = process.env
+dotenv.config();
 
 module.exports = {
-	solidity: "0.8.17",
-	networks: {
-		mumbai: {
-			url: ALCHEMY_API_URL,
-			accounts: [PRIVATE_ACCOUNT_KEY],
+	solidity: {
+		version: "0.8.17",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
 		},
 	},
-}
+	networks: {
+		scrollAlpha: {
+			url: "https://alpha-rpc.scroll.io/l2" || "",
+			accounts:
+				process.env.PRIV_KEY !== undefined ? [process.env.PRIV_KEY] : [],
+		},
+	},
+};
